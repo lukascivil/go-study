54a6c2c4d504771ca8a5c8f648a3c1f41849d60c
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2024-10-10 02:42:28 -0300

    Update main.go

--- main.go
+++ main.go
@@ -9,28 +9,8 @@ func main() {
 	// http.HandleFunc("/", getRoot)
 	// http.HandleFunc("/albums", getAlbums)
 	// http.HandleFunc("/fib", getFib)
-
-	// 1
-	// resp1, _ := http.Get("https://jsonplaceholder.typicode.com/posts")
-	// body1, _ := io.ReadAll(resp1.Body)
-	// json1 := string(body1)
-
-	// // fmt.Println(json1)
 	
+	responses := subjects.MakeRequests()
 
-	cafe := subjects.MakeRequests()
-
-	subjects.SaveJsonToFile(cafe, "requests")
-	
-	// fmt.Println("tamanho", cafe)
-
-
-	// err := http.ListenAndServe(":3333", nil)
-
-	// if errors.Is(err, http.ErrServerClosed) {
-	// 	fmt.Printf("server closed\n")
-	// } else if err != nil {
-	// 	fmt.Printf("error starting server: %s\n", err)
-	// 	os.Exit(1)
-	// }
+	subjects.SaveJsonToFile(responses, "requests")
 }
