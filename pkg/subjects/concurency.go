7aba1be16d76e611893f7786d42a5a88ca618f61
lucas cordeiro da Silva <lukas_civil@hotmail.com>
2024-10-10 13:22:54 -0300

    refactor: add simple concurrent request

--- pkg/subjects/concurency.go
+++ pkg/subjects/concurency.go
@@ -15,7 +15,7 @@ import (
  * - A channel to collect the responses;
  * - Goroutines to make the requests;
 **/ 
-func MakeRequests () [][]byte {
+func MakeRequestsWithWaitGroupAndChannel () [][]byte {
 	client := resty.New()
 	var wg = sync.WaitGroup{}
 	// Canal para coletar as respostas
@@ -69,3 +69,103 @@ func MakeRequests () [][]byte {
 
 	return allResponses
 }
+
+func MakeRequestsWithWaitGroup () [][]byte {
+	client := resty.New()
+	var wg = sync.WaitGroup{}
+	results := make([][]byte, 4)
+	
+	wg.Add(1)
+	go func () {
+		resp1, _ := client.R().Get("https://jsonplaceholder.typicode.com/posts/1")
+		fmt.Println(1)
+		results[0] = resp1.Body()
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func () {
+		resp2, _ := client.R().Get("https://jsonplaceholder.typicode.com/posts")
+		fmt.Println(2)
+		results[1] = resp2.Body()
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func () {
+		resp3, _ := client.R().Get("https://jsonplaceholder.typicode.com/posts")
+		fmt.Println(3)
+		results[2] = resp3.Body()
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func () {
+		resp4, _ := client.R().Get("https://jsonplaceholder.typicode.com/posts")
+		fmt.Println(4)
+		results[3] = resp4.Body()
+		wg.Done()
+	}()
+
+	fmt.Println("Waiting for all requests to finish...")
+	
+	// Wait for all goroutines to finish
+	wg.Wait()
+
+	return results
+}
+
+// func MakeRequestsWithErrGroup () [][]byte {
+// 	client := resty.New()
+// 	var wg = sync.WaitGroup{}
+// 	// Canal para coletar as respostas
+// 	responseChannel := make(chan []byte, 4)
+	
+// 	wg.Add(1)
+// 	go func () {
+// 		resp2, _ := client.R().Get("https://jsonplaceholder.typicode.com/posts/1")
+// 		fmt.Println(1)
+// 		responseChannel <- resp2.Body()
+// 		wg.Done()
+// 	}()
+
+// 	wg.Add(1)
+// 	go func () {
+// 		resp3, _ := client.R().Get("https://jsonplaceholder.typicode.com/posts")
+// 		fmt.Println(2)
+// 		responseChannel <- resp3.Body()
+// 		wg.Done()
+// 	}()
+
+// 	wg.Add(1)
+// 	go func () {
+// 		resp4, _ := client.R().Get("https://jsonplaceholder.typicode.com/posts")
+// 		fmt.Println(3)
+// 		responseChannel <- resp4.Body()
+// 		wg.Done()
+// 	}()
+
+// 	wg.Add(1)
+// 	go func () {
+// 		resp5, _ := client.R().Get("https://jsonplaceholder.typicode.com/posts")
+// 		fmt.Println(4)
+// 		responseChannel <- resp5.Body()
+// 		wg.Done()
+// 	}()
+
+// 	fmt.Println("Waiting for all requests to finish...")
+	
+// 	// Espera todas as goroutines terminarem
+// 	wg.Wait()
+// 	// Fecha o canal, pois todas as goroutines terminaram
+// 	close(responseChannel)
+
+
+// 	// Coletamos todas as respostas
+// 	var allResponses [][]byte
+// 	for response := range responseChannel {
+// 		allResponses = append(allResponses, response)
+// 	}
+
+// 	return allResponses
+// }
